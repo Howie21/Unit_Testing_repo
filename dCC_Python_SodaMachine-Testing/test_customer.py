1952abe53771d4aa0180aa88039aa6b348047301
Howie21 <bryce9643@gmail.com>
2021-10-20 13:03:30 -0400

    Addition and Finalization of the Customer Class Test File and test methods.

--- dCC_Python_SodaMachine-Testing/test_customer.py
+++ dCC_Python_SodaMachine-Testing/test_customer.py
@@ -1,6 +1,9 @@
 import unittest
 from customer import Customer
 from coins import Dime
+from cans import Cola
+from cans import OrangeSoda
+from cans import RootBeer
 
 class TestGetWalletCoin(unittest.TestCase):
     """Initail Customer Class for get_wallet_coin Method() testing"""
@@ -39,6 +42,7 @@ class TestAddCoinsToWallet(unittest.TestCase):
         self.customer = Customer()
 
     def test_length_of_money_list_adding(self):
+        """Test to ensure length of Customers Wallet Money List went up by three"""
         dime_1 = Dime()
         dime_2 = Dime()
         dime_3 = Dime()
@@ -51,6 +55,7 @@ class TestAddCoinsToWallet(unittest.TestCase):
         self.assertEqual((coins_list_length + 3), len(method_list))
 
     def test_length_of_money_list_not_changing(self):
+        """Test to ensure Customers wallet stays the same when passing a empty list"""
         coins_list = []
         coins_list_length = len(self.customer.wallet.money)
 
@@ -58,6 +63,37 @@ class TestAddCoinsToWallet(unittest.TestCase):
         method_list = self.customer.wallet.money
 
         self.assertEqual((coins_list_length), len(method_list))
+
+class TestAddCanToBackpack(unittest.TestCase):
+    """Testing Add_can_to_backpack to check if it alters the backpacks purchased cans list"""
+    def setUp(self):
+        self.customer = Customer()   
+
+    def test_adding_cola_to_backpack(self):
+        """Ensuring that Soda is added to the purchased_cans list in backpack"""
+        original = len(self.customer.backpack.purchased_cans)
+        cola = Cola()
+        self.customer.add_can_to_backpack(cola)
+        altered_value = len(self.customer.backpack.purchased_cans)
+        self.assertEqual((original + 1), altered_value)
+    
+    def test_adding_orange_soda_to_backpack(self):
+        """Ensuring that Orange Soda is added to the purchased_cans list in backpack"""
+        original = len(self.customer.backpack.purchased_cans)
+        cola = OrangeSoda()
+        self.customer.add_can_to_backpack(cola)
+        altered_value = len(self.customer.backpack.purchased_cans)
+        self.assertEqual((original + 1), altered_value)
+    
+    def test_adding_root_beer_to_backpack(self):
+        """Ensuring that Root Beer is added to the purchased_cans list in backpack"""
+        original = len(self.customer.backpack.purchased_cans)
+        cola = RootBeer()
+        self.customer.add_can_to_backpack(cola)
+        altered_value = len(self.customer.backpack.purchased_cans)
+        self.assertEqual((original + 1), altered_value)
+    
+
 
 if __name__ == '__main__':
     unittest.main()
