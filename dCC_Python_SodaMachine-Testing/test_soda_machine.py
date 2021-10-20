d818b5af658a6c9c2cc190880e5aadcf2cf6b6e7
andrew morales <andrew.morales725@gmail.com>
2021-10-20 13:50:23 -0400

    Added unit test class for soda machine class

---
+++ dCC_Python_SodaMachine-Testing/test_soda_machine.py
@@ -0,0 +1,48 @@
+from typing import Union
+import unittest
+from soda_machine import SodaMachine
+
+class TestFillRegister(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestFillInventory(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestGetCoinFromRegister(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestRegisterHasCoin(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestDetermineChangeValue(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestCalculateCoinValue(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestGetInventorySoda(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestReturnInventory(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+class TestDepositCoinsIntoRegister(unittest.TestCase):
+    """ """
+    def setUp(self):
+        self.soda_machine = SodaMachine()
